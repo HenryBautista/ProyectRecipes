4be3d1462b3148d7c144c6d7afa90a0e7d62196d
QHebert <hebertfqc81@hotmail.com>
2017-06-25 04:54:38 -0400

    General services change
    Basic general services is added to "GeneralServices.cs"

---
+++ CODIGO FUENTE/recipes/recipes/Util/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace recipes.Util
+{
+    public class Utilities
+    {
+        public static string DataName(string t_name)
+        {
+            string[] data = t_name.Split('_');
+            string name="";
+            foreach (string item in data)
+            {
+                name += item.First();
+            }
+            return name+t_name;
+        }
+    }
+}
