b71640260ee0aade1ca27ea21666fbea18ab063e
HenryBautista <HenryBautistaHTT@hotmail.com>
2017-06-29 18:13:57 -0400

    Register Recipes

---
+++ CODIGO FUENTE/recipes/recipes/Views/RegisterRecipeView.aspx.cs
@@ -0,0 +1,31 @@
+﻿using recipes.Services;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace recipes.Views
+{
+    public partial class RegisterRecipeView : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                load_data();
+            }
+        }
+
+        private void load_data()
+        {
+            DataTable turns = ConceptServices.GetConcept(1);
+            ddl_turn.DataSource = turns;
+            ddl_turn.DataBind();
+
+        }
+    }
+}
